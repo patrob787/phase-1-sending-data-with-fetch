28d09968c7dbbed63a1c73af59e39c9739e3c325
Patrick Roberts <patrob22@gmail.com>
2023-03-24 14:01:13 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then((obj) => {
+        let body = document.querySelector('body');
+        let id = obj.id;
+        body.append(id);
+    })
+    .catch((error) => {
+        let body = document.querySelector('body');
+        body.append(error.message);
+    })
+};
